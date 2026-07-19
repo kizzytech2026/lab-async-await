e483a3602e3f802e6258f7c1db851cb06ff90dc7
gerald Kizito <root@Kizzytech.localdomain>
2026-07-19 18:45:11 +0300

    Fixed Index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your code here!
+const postList = document.getElementById("post-list");
+
+function displayPosts(posts) {
+  posts.forEach((post) => {
+    const li = document.createElement("li");
+
+    const h1 = document.createElement("h1");
+    h1.textContent = post.title;
+
+    const p = document.createElement("p");
+    p.textContent = post.body;
+
+    li.appendChild(h1);
+    li.appendChild(p);
+
+    postList.appendChild(li);
+  });
+}
+
+async function fetchPosts() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+  const posts = await response.json();
+
+  displayPosts(posts);
+}
+
+fetchPosts();
